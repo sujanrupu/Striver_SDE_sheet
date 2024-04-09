ef5420e3f6b0dd92b6e74b5df3480a6d89fc002f
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-04-09 17:07:11 +0530

    Create Middle of the Linked List.cpp

---
+++ Linked List/Middle of the Linked List.cpp
@@ -0,0 +1,13 @@
+//Link: https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
