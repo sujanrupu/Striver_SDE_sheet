a105ae7093300a9d9fc041986cc2eb16fc8a546a
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-04-11 00:11:54 +0530

    Create K-th element of two Arrays.cpp

---
+++ Binary Search/K-th element of two Arrays.cpp
@@ -0,0 +1,15 @@
+//Link: https://www.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/1
+
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+        vector<int>ans;
+        for(int i=0; i<n; i++)
+            ans.push_back(arr1[i]);
+        for(int i=0; i<m; i++)
+            ans.push_back(arr2[i]);
+        sort(ans.begin(), ans.end());
+        return ans[k-1];
+    }
+};
