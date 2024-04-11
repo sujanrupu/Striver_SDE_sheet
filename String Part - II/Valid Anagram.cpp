26e65d39672f0574de9ead32291c649f3ae099e9
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-04-11 16:43:10 +0530

    Create Valid Anagram.cpp

---
+++ String Part - II/Valid Anagram.cpp
@@ -0,0 +1,10 @@
+//Link: https://leetcode.com/problems/valid-anagram/description/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
+    }
+};
