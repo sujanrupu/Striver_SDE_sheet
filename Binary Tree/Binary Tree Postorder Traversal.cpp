b47ad670642ef15471e487bf7141c2c90096ff9a
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-04-11 16:55:35 +0530

    Create Binary Tree Postorder Traversal.cpp

---
+++ Binary Tree/Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,18 @@
+//Link: https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int>& ans) {
+        if(!root)   return ;
+        solve(root->left, ans);
+        solve(root->right, ans);
+        ans.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(!root)   return ans;
+        solve(root, ans);
+        return ans;
+    }
+};
