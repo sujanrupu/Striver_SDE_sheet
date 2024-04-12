15ca0e83f3b9adfccb407a7aa1f3eb7063b6d6a1
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2024-04-12 19:57:17 +0530

    Create Inorder Predecessor of Node in BST.cpp

---
+++ Binary Search Tree/Inorder Predecessor of Node in BST.cpp
@@ -0,0 +1,14 @@
+//Link: https://workat.tech/problem-solving/practice/inorder-predecessor-bst
+
+Node* findPredecessor(Node* root, Node* p) {
+    Node *curr = root, *ans = NULL;
+	while(curr) {
+		if(curr->data < p->data) {
+			ans = curr;
+			curr = curr->right;
+		}
+		else
+			curr = curr->left;
+	}
+	return ans;
+}
